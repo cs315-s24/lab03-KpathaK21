ce58bfcabc9c7ccc57f4aa8f5986b86db9bcbeda
Kunj Pathak <kpathak3@dons.usfca.edu>
2024-02-19 09:20:10 +0000

    2/3

--- sumarr_rec_s.s
+++ sumarr_rec_s.s
@@ -3,34 +3,34 @@
 sumarr_rec_s:
     # Prologue
     addi    sp, sp, -16         # Adjust stack pointer
-    sd      ra, 8(sp)           # Save return address
-    sd      s0, 0(sp)           # Save s0
+    sd      ra, 0(sp)           # Save return address
+    sd      s0, 8(sp)           # Save s0
     
     # Base case: if start == end
-    beqz    a1, a2, base_case   # If start equals end, return arr[start]
+    beq     a1, a2, base_case   # If start equals end, return arr[start]
 
     # Recursive case
-    lw      s0, 0(a0)           # Load arr[start] into s0
-    addi    a1, a1, 1           # Increment start index
-    add     a0, a0, 4           # Move pointer to next element in the array
+    lw      s0, 0(a0)           # Load current element into s0
+    addi    a0, a0, 4           # Move pointer to next element in the array
+    addi    a1,a1, 1
     jal     ra, sumarr_rec_s    # Recursive call
 
-
-    # The sum of the current element and the sum of the rest of the array
-    add     s0, s0, t0          # s0 = arr[start] + sumarr_rec_s(arr, start + 1, end)
+    # Add current element to the sum
+    add     a0 , a0, s0          # s0 = arr[start] + sumarr_rec_s(arr, start + 1, end)
 
     # Epilogue
-    ld      ra, 8(sp)           # Restore return address
-    ld      s0, 0(sp)           # Restore s0
+    ld      ra, 0(sp)           # Restore return address
+    ld      s0, 8(sp)           # Restore s0
     addi    sp, sp, 16          # Restore stack pointer
     ret
 
+    
 base_case:
-    lw      s0, 0(a0)           # Load arr[start] into s0
+    ld      a0, 0(a0)           # Load arr[start] into s0
     j       end_sumarr_rec_s    # Jump to epilogue
 
 end_sumarr_rec_s:
-    ld      ra, 8(sp)           # Restore return address
-    ld      s0, 0(sp)           # Restore s0
+	ld      s0, 8(sp)           # Restore s0
+    ld      ra, 0(sp)           # Restore return address
     addi    sp, sp, 16          # Restore stack pointer
     ret
