79bc6b0c4f60f144d371cbe53b9cf8c8bcf9dc8b
KpathaK21 <kpathak3@dons.usfca.edu>
2024-02-20 22:48:17 +0000

    100,100

--- is_pal_rec_s.s
+++ is_pal_rec_s.s
@@ -2,22 +2,26 @@
 
 pal_rec_s:
 
-    beqz a1, return_true         # Single character string is a palindrome
-    bge a1, a2, return_true   # If a1 >= a2, all characters have been compared, and its a palindrome
+	bge a1, a2, return_true   # If a1 >= a2, all characters have been compared, and its a palindrome
+     
+  # Load characters from memory
+   add t0, a0, a1
+   lb t0, (t0)   # Load character from start index
+   add t1, a0, a2
+   lb t1, (t1)   # Load character from end index
 
+   
+  # Compare characters
+     bne t0, t1, return_false    # Characters dont match, not a palindrome
 
-        # Load characters from memory
-        lb t0, 0(a1)   # Load character from start index
-        lb t1, 0(a2)   # Load character from end index
+	beq t0, t1, return_true
 
-        # Compare characters
-        bne t0, t1, return_false    # Characters dont match, not a palindrome
-
-        # Move to the next characters
-        addi a1, a1, 1          # Move start index forward
-        addi a2, a2, -1         # Move end index backward
-        jal pal_rec_s          # Repeat the loop
-
+ 
+  # Move to the next characters
+     addi a1, a1, 1          # Move start index forward
+     addi a2, a2, -1         # Move end index backward
+     jal pal_rec_s          # Repeat the loop
+	
 return_true:
     li a0, 1                # return true
     ret
