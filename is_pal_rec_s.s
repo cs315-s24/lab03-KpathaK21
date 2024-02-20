bea3d57f574c7e2060e7e81b8791dfdec9e0475f
KpathaK21 <kpathak3@dons.usfca.edu>
2024-02-20 13:17:34 +0000

    87/100

--- is_pal_rec_s.s
+++ is_pal_rec_s.s
@@ -1,25 +1,28 @@
 .global pal_rec_s
 
-
 pal_rec_s:
-    # Base cases:
-    # If length is 0 or 1, string is palindrome 
-    beqz  a1, is_palindrome_true
-    bge a1, a2, is_palindrome_true    # Check if start >= end, if not, proceed to check palindrome
 
-    lbu t0, 0(a1)                  # Load a byte from the start pointer
-    lbu t1, 0(a2)                  # Load a byte from the end pointer
-    bne t0, t1, is_palindrome_false    # If the characters at start and end pointers are not equal, its not a palindrome
-    
-    addi a1, a1, 1                  # Move start pointer to next character
-    addi a2, a2, -1                 # Move end pointer to previous character
-    jal ra, pal_rec_s               # Recursively call pal_rec_s
-    
-    
-is_palindrome_false:
-    li a0, 0                        # Return 0 to indicate not palindrome
+    bge a1, a2, return_true     # If a2 < a1, then the string is empty or has one character, so its a palindrome
+    beqz a1, return_true         # Single character string is a palindrome
+
+    bge a1, a2, return_true   # If a1 >= a2, all characters have been compared, and its a palindrome
+        
+        # Load characters from memory
+        lb t0, 0(a1)   # Load character from start index
+        lb t1, 0(a2)   # Load character from end index
+
+        # Compare characters
+        bne t0, t1, return_false    # Characters dont match, not a palindrome
+
+        # Move to the next characters
+        addi a1, a1, 1          # Move start index forward
+        addi a2, a2, -1         # Move end index backward
+        jal pal_rec_s          # Repeat the loop
+
+return_true:
+    li a0, 1                # return true
     ret
-
-is_palindrome_true:
-    li a0, 1                        # If start > end, it means the string is empty or has one character, so its a palindrome
+    
+return_false:
+    li a0, 0                # return false
     ret
