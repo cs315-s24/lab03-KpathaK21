8c5bbf9179d483cb589d1b72c41c2f3e9569351d
KpathaK21 <kpathak3@dons.usfca.edu>
2024-02-20 13:33:11 +0000

    1 more to go

--- is_pal_rec_s.s
+++ is_pal_rec_s.s
@@ -2,11 +2,10 @@
 
 pal_rec_s:
 
-    bge a1, a2, return_true     # If a2 < a1, then the string is empty or has one character, so its a palindrome
     beqz a1, return_true         # Single character string is a palindrome
-
     bge a1, a2, return_true   # If a1 >= a2, all characters have been compared, and its a palindrome
-        
+
+
         # Load characters from memory
         lb t0, 0(a1)   # Load character from start index
         lb t1, 0(a2)   # Load character from end index
